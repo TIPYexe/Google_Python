46c583317aea09115642a8ef9f3604c7c5fc6d9f
Tipy <53595545+TIPYexe@users.noreply.github.com>
2020-12-10 16:05:40 +0200

    Update main.py
    Inca nu reusesc sa scot numele cutiilor. Dar sunt aproape.

--- tema4/main.py
+++ tema4/main.py
@@ -2,7 +2,7 @@ import json
 import requests
 from bs4 import BeautifulSoup as bs
 
-import web_scrapper as scrapper
+#import web_scrapper as scrapper
 
 URL = 'https://csgostash.com/containers/skin-cases'
 
@@ -10,10 +10,23 @@ if __name__ == '__main__':
     page = requests.get(URL)
     soup = bs(page.content, features='html.parser')
 
-    menu = soup.findAll('a', {'href':'#'})
-    #returenaza toate butoanele din meniu, care au un dropdown menu
+    ul_menu = soup.find('div', {'id':'navbar-expandable'}).find('ul')
+    ul_menu2 = ul_menu.findAll('li', {'class':'dropdown'})
 
-    dropdown_menu = menu.find('Cases')
-    print(dropdown_menu)
+    ok = 0
+    for buton in ul_menu2:
 
+        menu = buton.findAll('a', {'href':'#'})
+        for row in menu:
+            if(row.contents[0] == 'Cases'):
+                ok = 1
+                break
 
+        if ok == 1:
+            break
+
+    elem_menu = buton.findAll('li')
+
+    for name_case in elem_menu:
+        sub_cat = name_case.find('a')
+        print('SubCat: ', sub_cat, '\n')
