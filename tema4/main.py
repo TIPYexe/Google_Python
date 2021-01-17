5fda2661d0c7fde802b0f2fbb8a3a5dfc0ad569f
Tipy <53595545+TIPYexe@users.noreply.github.com>
2021-01-17 12:55:34 +0200

    Update main.py

--- tema4/main.py
+++ tema4/main.py
@@ -1,7 +1,7 @@
 import json
 import requests
 from bs4 import BeautifulSoup as bs
-
+import Case_Skins as cls
 import web_scrapper as scrapper
 
 URL = 'https://csgostash.com/containers/skin-cases'
@@ -11,26 +11,28 @@ if __name__ == '__main__':
     page = requests.get(URL)
     soup = bs(page.content, features='html.parser')
 
-    menu = soup.find('div', {'id':'navbar-expandable'}).find('ul')
-    all_buttons = menu.findAll('li', {'class':'dropdown'})
-
+    menu = soup.find('div', {'id': 'navbar-expandable'}).find('ul')
+    all_buttons = menu.findAll('li', {'class': 'dropdown'})
 
     ok = 0
     for button in all_buttons:
 
-        menu = button.findAll('a', {'href':'#'})
+        menu = button.findAll('a', {'href': '#'})
         for row in menu:
-            if(row.contents[0] == 'Cases'):
+            if (row.contents[0] == 'Cases'):
                 ok = 1
                 break
 
         if ok == 1:
             break
 
-    elem_menu = button.findAll('li') # accesez lista de elemente din dropdown_menu
-    elem_menu_set = set(elem_menu)   # o transform intr-o lista numarabila
+    elem_menu = button.findAll('li')  # accesez lista de elemente din dropdown_menu
+    elem_menu_set = set(elem_menu)  # o transform intr-o lista numarabila
 
-    (cases_names, cases_links) = scrapper.extract_data(elem_menu_set)
-
-    for link in cases_links:
-        scrapper.extract_skin_from(link)
+    Cases = []
+    for case_Link, case_Name in scrapper.extract_data(elem_menu_set):
+        ObjCase = cls.Case()
+        ObjCase.case_name = case_Name
+        ObjCase.link = case_Link
+        scrapper.extract_skin_from(ObjCase.link, ObjCase)
+        Cases.append(ObjCase)
