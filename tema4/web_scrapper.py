95bc355083943ac70aa97afeccb6a212ccd85b34
Tipy <53595545+TIPYexe@users.noreply.github.com>
2021-01-17 12:56:08 +0200

    Update web_scrapper.py

--- tema4/web_scrapper.py
+++ tema4/web_scrapper.py
@@ -2,16 +2,14 @@ import json
 import requests
 from selenium import webdriver
 from bs4 import BeautifulSoup as bs
+import Case_Skins as cls
 from selenium.webdriver.firefox.options import Options
 
 #region Extrag numele cutiilor
 
 
-def extract_case_name(data):
-    return data.find('img').get('alt')
-
-def extract_case_link(data):
-    return data.find('a').get('href')
+def extract_case_link_name(data):
+    return (data.find('a').get('href'), data.find('img').get('alt'))
 
 
 # pastrez doar diviziunile cu date despre cutii
@@ -31,16 +29,12 @@ def filter_cases(case_text):
         return True
 
 def extract_data(data):
-    #cols = ['Name', 'Link', 'Convert', 'Classified', 'Restricted', 'Mil-Spec']
     data_filtered = set(filter(filter_cases, data))
-    cases_names = set(map(extract_case_name, data_filtered))
-    cases_links = set(map(extract_case_link, data_filtered))
-
-    return (cases_names, cases_links)
+    return  set(map(extract_case_link_name, data_filtered))
 
 #endregion
 
-def extract_skin_from(URL):
+def extract_skin_from(URL, ObjCase):
     page = requests.get(URL)
     soup = bs(page.content, features='html.parser')
 
@@ -48,30 +42,13 @@ def extract_skin_from(URL):
 
     skin_boxes = soup.findAll('div', {'class': 'col-lg-4 col-md-6 col-widen text-center'})
 
+
     # nu iau prima fereastra pentru ca este cu cutite, si nu poti face trade cu ele
     for box in skin_boxes[1:]:
-        # extrage numele cu tot cu | dar merge bine in motorul de cautare de pe DMarket
-        skin_name = box.find('h3').text
-        skin_rarity = box.find('p', {'class': 'nomargin'}).text.split(' ')[0]
+        ObjSkin = cls.Skin()
 
-
-
-'''
-options = Options()
-options.add_argument('--headless')
-
-driver = webdriver.Firefox(options=options)
-driver.get("https://csgostash.com/case/308/Operation-Broken-Fang-Case")
-
-driver.find_element_by_class_name('qc-cmp-button').click()
-
-case_name = driver.find_element_by_xpath('/html/body/div[2]/div[3]/div/div[2]/h1').text
-
-#skin_boxes = driver.findAll('div', {'class':'col-lg-4 col-md-6 col-widen text-center'})
-box = driver.find_element_by_class_name('col-lg-4 col-md-6 col-widen text-center')
-
-#for box in skin_boxes:
-skin_name = box.find_element_by_css_selector('h3').find_elements_by_css_selector('a').__getattribute__('text')
-print(skin_name)
-'''
-
+        # extrage numele skinului si al armei, separat
+        ObjSkin.weapon = box.find('h3').text.split(" | ")[0]
+        ObjSkin.name = box.find('h3').text.split(" | ")[1]
+        ObjSkin.rarity = box.find('p', {'class': 'nomargin'}).text.split(' ')[0]
+        ObjCase.Skins.append(ObjSkin)
