441e692d29e720aa4c6a1ebe1c3d2c80f482ae1f
Tipy <53595545+TIPYexe@users.noreply.github.com>
2021-02-09 00:48:47 +0200

    Update main.py

--- tema4/main.py
+++ tema4/main.py
@@ -3,6 +3,7 @@ import requests
 from bs4 import BeautifulSoup as bs
 import Case_Skins as cls
 import web_scrapper as scrapper
+import dmarket_api as api
 
 URL = 'https://csgostash.com/containers/skin-cases'
 
@@ -29,10 +30,32 @@ if __name__ == '__main__':
     elem_menu = button.findAll('li')  # accesez lista de elemente din dropdown_menu
     elem_menu_set = set(elem_menu)  # o transform intr-o lista numarabila
 
+    print("Gata extractia din meniu")
+
     Cases = []
     for case_Link, case_Name in scrapper.extract_data(elem_menu_set):
         ObjCase = cls.Case()
-        ObjCase.case_name = case_Name
+        ObjCase.name = case_Name
         ObjCase.link = case_Link
         scrapper.extract_skin_from(ObjCase.link, ObjCase)
         Cases.append(ObjCase)
+
+    print("Gata extractia din cutii")
+
+    # Lista de cutii si procentul pe care sa nu il depaseasca un skin
+    # (daca skin-ul valoreaza 10$ si cineva se gaseste sa il vanda cu un pret iesit din comun rau de tot, gen 20$
+    # sa nu il luam in calcul)
+    api.insert_offer_to_DB(Cases, 10)
+
+    #'''
+    for case in set(Cases):
+        print("Case Name: " + case.name)
+        print("Link: " + case.link)
+        for skin in case.Skins:
+            print("Weapon: " + skin.weapon + " | Skin: " + skin.name)
+            print("FN " + skin.extra['FN'])
+            print("FT " + skin.extra['FT'])
+            print("SFN " + skin.extra['SFN'])
+        print()
+    #'''
+
