e72573f875f7a45a194b8419b38faf68b9752a51
Tipy <53595545+TIPYexe@users.noreply.github.com>
2021-02-09 00:48:29 +0200

    Update Case_Skins.py

--- tema4/Case_Skins.py
+++ tema4/Case_Skins.py
@@ -1,19 +1,30 @@
-
-# pentru fiecare calitate stim pretul
-class Skin():
+class Extra:
     def __init__(self):
+        self.FN = -1  # 0
+        self.MW = -1  # 1
+        self.FT = -1  # 2
+        self.WW = -1  # 3
+        self.BS = -1  # 4
+        self.SFN = -1  # 5  # cele cu S in fata au StatTrak (valoreaza mai mult)
+        self.SMW = -1  # 6
+        self.SFT = -1  # 7
+        self.SWW = -1  # 8
+        self.SBS = -1  # 9
+
+
+# cele cu S in fata au StatTrak (valoreaza mai mult)
+# pentru fiecare calitate stim pretul
+class Skin:
+    def __init__(self):
+        self.extra = Extra
         self.name = ""
         self.weapon = ""
         self.rarity = ""
-        self.FN = 0
-        self.MW = 0
-        self.FT = 0
-        self.WW = 0
-        self.BS = 0
+
 
 # fiecare cutie cu skin-uri + link-ul cu date despre skin-urile din ea
-class Case():
+class Case:
     def __init__(self):
-        self.case_name = ""
+        self.name = ""
         self.link = ""
         self.Skins = []
