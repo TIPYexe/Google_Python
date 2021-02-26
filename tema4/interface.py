e5f8dbde1c77de32fb00ab7badc1830b6d264394
Tipy <53595545+TIPYexe@users.noreply.github.com>
2021-02-26 23:21:38 +0200

    Update interface.py

--- tema4/interface.py
+++ tema4/interface.py
@@ -1,77 +1,109 @@
 import tkinter as tk
 from PIL import ImageTk, Image
-import web_scrapper as cs_scrp
+from pylightxl import readxl
+
+import price_scrapper as pricey
 import Case_Skins as cls
+import web_scrapper as cs_scrp
+import get_offer_alg as alg
+import save_to_db as save
+
 
 def updateDisplay(myString):
     displayVar.set(displayVar.get() + '\n' + myString)
 
-def log_viewer(event, p):
-    displayLab.after(1000, updateDisplay(p))
 
 def extract_skins(event):
-    Cases = cs_scrp.extract_from_cases('https://csgostash.com/containers/skin-cases', displayVar)
+    # dezactivez butonul
+    btn_UpdateDB['state'] = 'disabled'
+    btn_UpdateDB.unbind("<Button-1>")
+    # curat fereastra de log
+    displayVar.set('')
+    window.update()
 
-def printCases(event):
-    for case in Cases:
-        for skin in case.Skins:
-            print(case.name, skin.name, skin.weapon)
+    # functia propriu-zisa
+    Cases = cs_scrp.extract_from_cases('https://csgostash.com/containers/skin-cases', displayVar, window)
+    save.to_xlsx(Cases, 'Files/Skin_names.xlsx', False)
 
-skin_names_file = 'Files/test4.txt'
-skin_prices_file = 'Files/test5.txt'
+    # reactivez butonul
+    btn_UpdateDB['state'] = 'active'
+    btn_UpdateDB.bind("<Button-1>", extract_skins)
+
+
+def extract_prices(event):
+    # dezactivez butonul
+    btn_UpdatePrices['state'] = 'disabled'
+    btn_UpdatePrices.unbind("<Button-1>")
+    # curat fereastra de log
+    displayVar.set('')
+    window.update()
+
+    # functia propriu-zisa
+    db = readxl('Files/Skin_names.xlsx')
+    sheet_names = db.ws_names
+
+    for sheet in sheet_names[1:]:
+        ObjCase = cls.Case()
+        ObjCase.name = sheet
+        alg.read_xl(db, sheet, ObjCase)
+        Cases.append(ObjCase)
+
+    pricey.extract_prices('Files/Skin_names.xlsx', 'Files/Skin_prices.xlsx', displayVar, window)
+
+    # reactivez butonul
+    btn_UpdatePrices['state'] = 'active'
+    btn_UpdatePrices.bind("<Button-1>", extract_prices)
+
+# TODO:
+#   - sa fac si eu un fisier pentru constante (nume de fisiere and shit)
 
 window = tk.Tk()
+window.geometry('825x733')
 window.title("CS:GO Trade-up Scrapper")
+window.resizable(False, False)
 
 path_logo = 'Files/logo.png'
 
-# Pregatesc variabile pentru textul din cele mai de sus butoane din meniu
-# pentru ca vreau sa il pot schimba atunci cand operatia este finalizata
-btn_load_names_txt = tk.StringVar()
-btn_prices_txt = tk.StringVar()
+pixel = tk.PhotoImage(width=1, height=1)
 
-frame1 = tk.Frame(master=window, height=300, bg='black')
-frame1.pack(fill=tk.X, side=tk.TOP)
+frame = tk.Frame(bg='gray15', height=733, width=825)
+frame.pack()
 
-frame2 = tk.Frame(master=window, height=200, bg='white')
-frame2.pack(fill=tk.Y, side=tk.LEFT)
+# log_viewer = tk.Frame(master=window, weight=354.49, height=160.93, bg='white')
+# log_viewer.pack(fill=tk.Y, side=tk.RIGHT)
 
 image = Image.open(path_logo)
-image = image.resize((300, 197))
+#image = image.resize((300, 197))
 logo = ImageTk.PhotoImage(image)
-
-btn_load_names_txt.set("Load\nCases & Skins")
-btn_prices_txt.set("Extract\nSkin Prices")
 
 # este o carpeala de nedescris cu asezarea butoanelor astora
 # nu te uita
-logo_panel = tk.Label(frame1, image=logo, bd=0, highlightbackground='black')
-logo_panel.grid(row=0, column=0, padx=(70, 70), pady=20)
+logo_panel = tk.Label(frame, image=logo, bd=0)
+logo_panel.place(x=258, y=13)
 
-btn_UpdateDB = tk.Button(frame1, textvariable=btn_load_names_txt, bd=2, background='cornflower blue')
-btn_UpdateDB.grid(padx=(30, 250), pady=(0, 10), ipadx=50, ipady=10, row=1, column=0, sticky='W')
+btn_UpdateDB = tk.Button(frame, text="EXTRACT\nNAMES", image=pixel, bd=0, background='gray63', fg='gray34', font=('Montserrat Black', 12), width=153, height=63, compound="c")
+btn_UpdateDB.place(x=47, y=209)
 
-btn_UpdatePrices = tk.Button(frame1, textvariable=btn_prices_txt, bd=0)
-btn_UpdatePrices.grid(padx=(250, 30), pady=(0, 10), ipadx=50, ipady=10, row=1, column=0, sticky='W')
+btn_UpdatePrices = tk.Button(frame, text="EXTRACT\nPRICES", image=pixel, bd=0, background='gray63', fg='gray34', font=('Montserrat Black', 12), width=153, height=63, compound="c")
+btn_UpdatePrices.place(x=47, y=289)
 # dada, vezi bine
 # sunt pe aceeasi coloana 2 butoane
+
 
 displayVar = tk.StringVar()
 # justify = left (aliniez textul la stanga)
 # anchor = 's' (ultimul text introdus ramane in josul paginii si oricum redimensionez Label-ul
 #               el ramane jos de tot)
 # height = 10 (inaltimea log viewer-ului sa fie de 10)
-displayLab = tk.Label(frame2, textvariable=displayVar, justify='left', anchor='s', height=10)
-displayLab.grid()
+# displayLab = tk.Label(window, textvariable=displayVar, height=161, width=355)
+# displayLab.place(x=212, y=209)
 
-#btn_UpdatePrices.bind("<Button-1>", lambda event, p='TEXT NOU': log_viewer(event, p))
-#btn_UpdateDB.bind("<Button-1>", lambda event, a='': displayVar.set(a))
+# btn_UpdatePrices.bind("<Button-1>", lambda event, p='TEXT NOU': log_viewer(event, p))
+# btn_UpdateDB.bind("<Button-1>", lambda event, a='': displayVar.set(a))
 
 Cases = []
 btn_UpdateDB.bind("<Button-1>", extract_skins)
-
-btn_UpdatePrices.bind("<Button-1>", printCases)
-
+btn_UpdatePrices.bind("<Button-1>", extract_prices)
 
 # TODO:
 #   - la catergoria de get offer sa am 3-5 butoane care sa reprezinte
@@ -83,11 +115,6 @@ btn_UpdatePrices.bind("<Button-1>", printCases)
 # TODO:
 #   - sa incarc si texturile skin-urilor, iar atungi cand se alege o oferta, sa fie si afisata ca intr-un contract
 #     pe CS:GO
-#   - imi va trebui un webscrapper care sa caute rapid skin-urile (daca nu le are deja) din oferta pe csgo stash
-#   si sa le descarce imaginile, iar apoi sa le foloseasca
 
 
 window.mainloop()
-
-
-
